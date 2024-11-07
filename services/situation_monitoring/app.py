7e5bdb2307de658f4891fc178dae95cfe1864636
Sabari Narayana <sabarinarayana@Sabaris-MacBook-Air.local>
2024-11-07 16:30:52 +0530

    some servicers are updated for accountconfig

--- services/situation_monitoring/app.py
+++ services/situation_monitoring/app.py
@@ -2,6 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def load_json_file(filepath):
+    with open(filepath) as f:
+        return json.load(f)
+
+# Load JSON configuration
+json_file = os.getenv("CONFIG_FILE", "config.json")  # Default to config.json
+config = load_json_file(json_file)
+
+cred = credentials.Certificate(config['firebase'])
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 @app.route('/')
 def home():
     return "Hello, World! Welcome to my Flask app! Situation Monitoring"
